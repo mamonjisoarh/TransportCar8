b91f9f8fad75947bef2d1efe9ab0e77d67eb1ae7
mamonjisoarh <mamonjisoa.rh@gmail.com>
2026-08-20 06:44:36 +0300

    Update script.js

--- script.js
+++ script.js
@@ -5,7 +5,7 @@
     var isBusy = false; // vrai pendant le traitement d'un scan (évite les doublons)
     var isRunning = false; // vrai tant que la caméra scanne activement
     var selectedMode = 'entree';
-    var API_URL = (typeof window !== 'undefined' && window.API_URL) || 'https://script.google.com/macros/s/AKfycbyEBMufa2qRJFxTG1X2WH33COMM-eyJK5QUxoO3eSsLoarY0tIpLUo1OfYerkBww34s/exec';
+    var API_URL = (typeof window !== 'undefined' && window.API_URL) || 'https://script.google.com/macros/s/AKfycbxrd33qprtH7vusqlCDgauYc2TkFsletyavmj0kaCHAy8jWwWULbfv3eYythAXEDer4kg/exec';
     var config = { fps: 12, qrbox: { width: 250, height: 250 } };
 
     function setMode(mode) {
